cb5f085178327fd056bdb0643c5726a124346599
Temitope S <temitope@gmail.com>
2015-12-27 15:56:08 -0800

    added sound capability to alert/alarm at interval notification

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,11 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var ionicApp = angular.module('starter', ['ionic','ngCordova'])
-
+var ionicApp = angular.module('starter', ['ionic','ngCordova']);
+var media;
+var src= "beep.wav";
 ionicApp.run(function($ionicPlatform) {
+  
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -15,30 +17,34 @@ ionicApp.run(function($ionicPlatform) {
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+
   });
 });
 
 var hi = 14;
 var start = 0;
-ionicApp.controller('myCtrl', function($scope , $cordovaVibration, $cordovaMedia){
-    var mediaStatusCallback = function(status) {
-        if(status == 1) {
-            $ionicLoading.show({template: 'Loading...'});
-        } else {
-            $ionicLoading.hide();
-        }
-    }
-    var src= "beep.wav";
-    console.log(src);
-    console.log(JSON.stringify($cordovaMedia));
-    var media;
-    try{
-      media = new Media(src, null, null, mediaStatusCallback);
-    }catch(ex){
-      console.log("error " + ex);
-    }
+ionicApp.controller('myCtrl', function($scope , $cordovaVibration, $cordovaMedia, $cordovaDevice){
+    
     $scope.toggle = function(){
-      
+          /*var mediaStatusCallback = function(status) {
+              if(status == 1) {
+                  $ionicLoading.show({template: 'Loading...'});
+              } else {
+                  $ionicLoading.hide();
+              }
+          }*/
+          //if (device.platform == 'Android') {
+           //   src = '/android_asset/www/' + src;  // Android needs the search path explicitly specified
+          //}
+          console.log(src);
+          console.log(JSON.stringify($cordovaMedia));
+          
+          try{
+            media = new Media(src, null, null, null);
+          }catch(ex){
+            console.log("error " + ex);
+          }
       
 
       start++;
@@ -51,7 +57,10 @@ ionicApp.controller('myCtrl', function($scope , $cordovaVibration, $cordovaMedia
         var trip = 0;
 
         $cordovaVibration.vibrate(500);//vibrate off bat, then 2 more will come
-        //media.play();
+        if(media)
+          media.play();
+        else
+          console.log("no media var avail");
         var trip_interval = setInterval(function(){
             if(trip<2){
               $cordovaVibration.vibrate(500);
